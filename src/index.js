20915ecf00d0330d5e360fba82eab2cfb0483405
abderrahim <info.abderrahimtijani@gmail.com>
2021-02-05 17:10:42 +0100

    add content hash feature to avoide  browser cache

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./style.scss";
 const mybutton = document.getElementById("myBtn");
 
 
-// When the user scrolls down 20px from the top of the document, show the button
+// When the user scrolls down 20px from the top of the document, shows the btn
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -15,12 +15,14 @@ function scrollFunction() {
   }
 }
 
-
 // When the user clicks on the button, scroll to the top of the document
 
 mybutton.addEventListener('click', () => {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 })
+
+
+
 
 
